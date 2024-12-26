b151aba4b8b238b241ffbcb2db46691395ffec3f
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-12-26 19:10:06 +0000

    create the login form

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,34 +1,8 @@
-'use client';
-
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import Image from 'next/image';
-import { MoveRight } from 'lucide-react';
-import { useForm } from 'react-hook-form';
-import { defaultValues, LoginInputSchema, schema } from '../types/schema';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useLoginUser } from '../services/mutations';
-import { ClipLoader } from 'react-spinners';
 import Head from 'next/head';
+import LoginForm from '../_components/login-form';
 
 export default function Login() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<LoginInputSchema>({
-    mode: 'all',
-    resolver: zodResolver(schema),
-    defaultValues,
-  });
-
-  const { mutate: loginUser, isPending } = useLoginUser();
-
-  const onFormSubmit = (data: LoginInputSchema) => {
-    loginUser(data);
-  };
-
   return (
     <>
       <Head>
@@ -61,50 +35,7 @@ export default function Login() {
               <h1 className="text-4xl font-bold text-primary">Welcome back</h1>
               <p className="">Sign in to your account to continue</p>
             </div>
-            <form
-              onSubmit={handleSubmit(onFormSubmit)}
-              className="mt-8 space-y-4"
-            >
-              <div>
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  type="email"
-                  id="email"
-                  {...register('email')}
-                  error={!!errors.email}
-                  helperText={errors.email?.message}
-                  placeholder="Enter your email"
-                />
-              </div>
-              <div>
-                <Label htmlFor="password">Password</Label>
-                <Input
-                  type="password"
-                  id="password"
-                  {...register('password')}
-                  error={!!errors.password}
-                  helperText={errors.password?.message}
-                  placeholder="Enter your password"
-                />
-              </div>
-              <div>
-                <Button size="lg" className="w-full gap-6">
-                  {isPending ? (
-                    <>
-                      <span>Please wait</span>
-                      <ClipLoader color="#fff" size={24} />
-                    </>
-                  ) : (
-                    <>
-                      <span>Continue</span>
-                      <span>
-                        <MoveRight />
-                      </span>
-                    </>
-                  )}
-                </Button>
-              </div>
-            </form>
+            <LoginForm />
           </div>
         </div>
       </main>
