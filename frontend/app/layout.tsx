5a88fbe8f663eb6e649d957bebb3ff64f4958970
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-12-26 05:15:46 +0000

    install and configure react-query

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import Providers from './providers';
 
 const poppins = Poppins({
   weight: ['100', '200', '500', '600', '400', '700', '800'],
@@ -22,7 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
