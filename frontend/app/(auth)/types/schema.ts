f3741d257b3d885ae1ace3459cba4890e38f8092
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-12-26 19:04:47 +0000

    create schema for auth

--- frontend/app/(auth)/types/schema.ts
+++ frontend/app/(auth)/types/schema.ts
@@ -1,7 +1,7 @@
 // import { patterns } from '@/lib/utils';
 import { z } from 'zod';
 
-export const schema = z.object({
+export const loginSchema = z.object({
   email: z
     .string()
     .min(1, { message: 'Email is required' })
@@ -16,10 +16,33 @@ export const schema = z.object({
   // remember: z.boolean(),
 });
 
-export type LoginInputSchema = z.infer<typeof schema>;
+export type LoginInputSchema = z.infer<typeof loginSchema>;
 
 export const defaultValues: LoginInputSchema = {
   email: '',
   password: '',
   // remember: false,
 };
+
+// user schema
+export const userSchema = z.object({
+  user_id: z.string(),
+  firstname: z.string(),
+  lastname: z.string(),
+  username: z.string(),
+  email: z.string(),
+  role: z.enum(['user', 'admin']),
+});
+
+// user schema type
+export type UserSchema = z.infer<typeof userSchema>;
+
+// session schema
+export const sessionSchema = z.object({
+  accessToken: z.string(),
+  refreshToken: z.string(),
+  user: userSchema,
+});
+
+// session schema type
+export type SessionSchema = z.infer<typeof sessionSchema>;
