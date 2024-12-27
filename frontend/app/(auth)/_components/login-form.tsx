93e42739d819b08bbe8c48217f7f7023fa8cf5b1
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-12-27 09:24:16 +0000

    update login

--- frontend/app/(auth)/_components/login-form.tsx
+++ frontend/app/(auth)/_components/login-form.tsx
@@ -30,74 +30,75 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onFormSubmit)} className="p-6 md:p-8">
+    <div className="p-6 md:p-8">
       <div className="flex justify-center">
         <AppLogo width={80} />
       </div>
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-col items-center text-center">
-          <h1 className="text-2xl font-semibold">Welcome back</h1>
-          <p className="text-balance text-muted-foreground">
-            Login to your CIT account
-          </p>
-        </div>
+      <div className="flex flex-col items-center text-center">
+        <h1 className="text-2xl font-semibold">Welcome back</h1>
+        <p className="text-balance text-muted-foreground">
+          Login to your CIT account
+        </p>
+      </div>
+      <form onSubmit={handleSubmit(onFormSubmit)}>
+        <div className="flex flex-col gap-6">
+          <div>
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              id="email"
+              {...register('email')}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+              placeholder="Enter your email"
+            />
+          </div>
+          <div>
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link
+                href="#"
+                className="ml-auto text-sm underline-offset-2 hover:underline"
+              >
+                Forgot your password?
+              </Link>
+            </div>
+            <Input
+              type="password"
+              id="password"
+              {...register('password')}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              placeholder="Enter your password"
+            />
+          </div>
+          <div>
+            <Button size="lg" className="w-full gap-6">
+              {isPending ? (
+                <>
+                  <span>Please wait</span>
+                  <ClipLoader color="#fff" size={24} />
+                </>
+              ) : (
+                <>
+                  <span>Continue</span>
+                  <span>
+                    <MoveRight />
+                  </span>
+                </>
+              )}
+            </Button>
+          </div>
 
-        <div>
-          <Label htmlFor="email">Email</Label>
-          <Input
-            type="email"
-            id="email"
-            {...register('email')}
-            error={!!errors.email}
-            helperText={errors.email?.message}
-            placeholder="Enter your email"
-          />
-        </div>
-        <div>
-          <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-            <Link
-              href="#"
-              className="ml-auto text-sm underline-offset-2 hover:underline"
-            >
-              Forgot your password?
+          <div className="text-center text-sm">
+            Don&apos;t have an account?{' '}
+            <Link href="#" className="underline underline-offset-4">
+              Contact support
             </Link>
           </div>
-          <Input
-            type="password"
-            id="password"
-            {...register('password')}
-            error={!!errors.password}
-            helperText={errors.password?.message}
-            placeholder="Enter your password"
-          />
         </div>
-        <div>
-          <Button size="lg" className="w-full gap-6">
-            {isPending ? (
-              <>
-                <span>Please wait</span>
-                <ClipLoader color="#fff" size={24} />
-              </>
-            ) : (
-              <>
-                <span>Continue</span>
-                <span>
-                  <MoveRight />
-                </span>
-              </>
-            )}
-          </Button>
-        </div>
-
-        <div className="text-center text-sm">
-          Don&apos;t have an account?{' '}
-          <Link href="#" className="underline underline-offset-4">
-            Contact support
-          </Link>
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -12,7 +12,7 @@ export default function Login() {
       </Head>
 
       <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-        <div className="w-full max-w-sm md:max-w-3xl">
+        <div className="w-full max-w-sm md:max-w-[52rem]">
           <div className="flex flex-col gap-6">
             <Card className="overflow-hidden">
               <CardContent className="grid p-0 md:grid-cols-2">
