7e71d626aeb4761436867d3e62142f0c75a39f93
Pristine Inc GH <pristineincgh@gmail.com>
2025-02-05 15:03:24 +0000

    add validations

--- admin-app/src/lib/validations/auth_validations.ts
+++ admin-app/src/lib/validations/auth_validations.ts
@@ -1,11 +1,11 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 // Schema for validating login request data
 export const loginRequestSchema = z.object({
-  username: z.string().min(1, { message: 'Username is required' }),
+  username: z.string().min(1, { message: "Username is required" }),
   password: z
     .string()
-    .min(8, { message: 'Password must be at least 8 characters' }),
+    .min(8, { message: "Password must be at least 8 characters" }),
 });
 
 // Type for login request input
@@ -13,6 +13,23 @@ export type LoginRequestInput = z.infer<typeof loginRequestSchema>;
 
 // Default values for login request
 export const defaultLoginValues: LoginRequestInput = {
-  username: '',
-  password: '',
+  username: "",
+  password: "",
 };
+
+// Schema for validating password reset request data
+export const resetPasswordSchema = z
+  .object({
+    token: z.string(),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters" }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"], // This will attach the error to the confirmPassword field
+  });
+
+// Type for password reset request input
+export type ResetPasswordInput = z.infer<typeof resetPasswordSchema>;
--- admin-app/src/lib/validations/customer_validations.ts
+++ admin-app/src/lib/validations/customer_validations.ts
@@ -1,27 +1,27 @@
-import { z } from 'zod';
+import { z } from "zod";
+import { PhoneNumberUtil } from "google-libphonenumber";
+
+// Initialize PhoneNumberUtil instance
+const phoneUtil = PhoneNumberUtil.getInstance();
+
+const isPhoneValid = (phone: string) => {
+  try {
+    return phoneUtil.isValidNumber(phoneUtil.parseAndKeepRawInput(phone));
+  } catch {
+    return false;
+  }
+};
 
 export const customerFormSchema = z.object({
   name: z
-    .string({ required_error: 'Name is required' })
-    .nonempty('Name is required'),
+    .string({ required_error: "Name is required" })
+    .nonempty("Name is required"),
   phone_number: z
     .string()
-    .nonempty('Phone number is required')
-    .refine(
-      (value) => {
-        // Remove any spaces or special characters
-        const cleanPhone = value.replace(/\s+/g, '');
-
-        // Pattern for numbers starting with 233
-        const pattern233 = /^233(20|23|24|25|26|27|50|54|55|56|57|58|59)\d{7}$/;
-
-        // Pattern for numbers starting with 0
-        const pattern0 = /^0(20|23|24|25|26|27|50|54|55|56|57|58|59)\d{7}$/;
-
-        return pattern233.test(cleanPhone) || pattern0.test(cleanPhone);
-      },
-      {
-        message: 'Phone number must be a valid number',
-      }
-    ),
+    .nonempty({
+      message: "Please enter a phone number.",
+    })
+    .refine((phone) => isPhoneValid(phone), {
+      message: "Please enter a valid phone number.",
+    }),
 });
