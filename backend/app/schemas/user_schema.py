a2f8a5573983fc6133edff7d8e36410910e420b7
Pristine Inc GH <pristineincgh@gmail.com>
2025-02-05 14:56:29 +0000

    update user schema

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -29,7 +29,6 @@ class UserInCreate(BaseModel):
     last_name: str = Field(..., min_length=2, max_length=50)
     username: str = Field(..., min_length=2, max_length=50)
     email: EmailStr
-    password: str = Field(..., min_length=8, max_length=50)
     role: Role = Role.AGENT
 
 
@@ -44,72 +43,12 @@ class UserOutput(BaseModel):
     username: str
     email: EmailStr
     role: Role
+    login_count: int
     is_active: bool
-    is_verified: bool
     created_at: datetime
     updated_at: datetime
 
     model_config = {"from_attributes": True}
-
-
-class UserInUpdate(BaseModel):
-    """
-    This class is used to validate the user data when updating a user.
-    """
-
-    id: str
-    first_name: Optional[str] = Field(None, min_length=2, max_length=50)
-    last_name: Optional[str] = Field(None, min_length=2, max_length=50)
-    username: Optional[str] = Field(None, min_length=2, max_length=50)
-    email: Optional[EmailStr] = None
-    password: Optional[str] = Field(None, min_length=8, max_length=50)
-    role: Optional[Role] = None
-    is_active: Optional[bool] = None
-    is_verified: Optional[bool] = None
-
-
-class UserInLogin(BaseModel):
-    """
-    This class is used to validate the user data when logging in.
-    """
-
-    username: str = Field(..., min_length=2, max_length=50)
-    password: str = Field(..., min_length=8, max_length=50)
-
-
-class UserWithToken(BaseModel):
-    """
-    This class is used to return access and refresh tokens after a successful login.
-    """
-
-    access_token: str
-    refresh_token: str
-
-
-class UserLoginResponse(UserWithToken):
-    """
-    This class is used to return access and refresh tokens after a successful login.
-    """
-
-    user: UserOutput
-
-
-class TokenRefreshRequest(BaseModel):
-    refresh_token: str
-
-
-class UserNewTokens(UserWithToken):
-    """
-    This class is used to return a new access token after a successful refresh.
-    """
-
-
-class UserLogout(BaseModel):
-    """
-    This class is used to send success response.
-    """
-
-    message: str
 
 
 class UserListResponse(BaseModel):
@@ -121,3 +60,17 @@ class UserListResponse(BaseModel):
     users: list[UserOutput]
 
     model_config = {"from_attributes": True}
+
+
+class UserInUpdate(BaseModel):
+    """
+    This class is used to validate the user data when updating a user.
+    """
+
+    first_name: Optional[str] = Field(None, min_length=2, max_length=50)
+    last_name: Optional[str] = Field(None, min_length=2, max_length=50)
+    username: Optional[str] = Field(None, min_length=2, max_length=50)
+    email: Optional[EmailStr] = None
+    password: Optional[str] = Field(None, min_length=8, max_length=50)
+    role: Optional[Role] = None
+    is_active: Optional[bool] = None
