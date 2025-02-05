6de42f3f55c249319348378c4a5171e14e4f7e94
Pristine Inc GH <pristineincgh@gmail.com>
2025-02-05 15:04:23 +0000

    update dashboard layout

--- admin-app/src/app/dashboard/layout.tsx
+++ admin-app/src/app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
-import { getCurrentUser } from '@/actions/auth-actions';
-import NavBar from '@/components/dashboard/navigation/navbar';
-import { redirect } from 'next/navigation';
+import { getCurrentUser } from "@/actions/auth-actions";
+import NavBar from "@/components/dashboard/navigation/navbar";
+import { redirect } from "next/navigation";
 
 const RootLayout = async ({
   children,
@@ -10,13 +10,13 @@ const RootLayout = async ({
   const user = await getCurrentUser();
 
   if (!user) {
-    redirect('/login');
+    redirect("/login");
   }
 
   return (
     <div className="3xl:w-[1500px] 3xl:mx-auto 3xl:border">
       <NavBar />
-      <main className="p-5">{children}</main>
+      <main>{children}</main>
     </div>
   );
 };
