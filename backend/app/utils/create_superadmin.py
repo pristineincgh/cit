87dc3f128b00af657a2fb7dcf20efa1977b54532
Pristine Inc GH <pristineincgh@gmail.com>
2025-02-05 14:54:10 +0000

    create superadmin if it does not exist

--- backend/app/utils/create_superadmin.py
+++ backend/app/utils/create_superadmin.py
@@ -1,37 +1,51 @@
-from sqlalchemy.orm import Session
-from app.db.models import User
-from app.db.database import SessionLocal
-from app.services.user_service import UserService
-from app.schemas.user_schema import Role, UserInCreate
 from app.core.config import get_settings
+from app.core.security.hash_helper import HashHelper
+from app.db.database import SessionLocal
+from app.db.models.user_model import User
+from app.schemas.user_schema import Role, UserInCreate
 
 settings = get_settings()
+
 
 async def create_superadmin_if_not_exists():
     # Initialize a database session
     with SessionLocal() as db:
-        # Manually initialize UserService with the database session
-        service = UserService(session=db)
-        
-        # Check if superadmin exists
-        superadmin = db.query(User).filter(User.role == Role.SUPERADMIN).first()
-        if superadmin:
-            print("Superadmin already exists")
-            return
-
-        # Create superadmin if not exists
-        user_data = UserInCreate(
-            first_name="Pristine",
-            last_name="Superadmin",
-            username="superadmin",
-            email="admin@pristineincgh.com",
-            password=settings.admin_password,  # Ensure this is a secure hashed password
-            role=Role.SUPERADMIN,
-        )
 
         try:
-            # Use the UserService to sign up the superadmin
-            service.signup(user_data)
+            # Check if superadmin exists
+            superadmin = db.query(User).filter(User.role == Role.SUPERADMIN).first()
+            if superadmin:
+                print("Superadmin already exists")
+                return
+
+            # Create superadmin if not exists
+            user_data = UserInCreate(
+                first_name="Pristine",
+                last_name="Superadmin",
+                username="superadmin",
+                email="admin@pristineincgh.com",  # Ensure this is a secure hashed password
+                role=Role.SUPERADMIN,
+            )
+
+            # Hash the user's password
+            hash_pass = HashHelper.get_password_hash(settings.admin_password)
+
+            new_user_data = {
+                "first_name": user_data.first_name,
+                "last_name": user_data.last_name,
+                "email": user_data.email,
+                "username": user_data.username,
+                "password": hash_pass,
+                "role": user_data.role,
+                "login_count": 1,
+            }
+
+            # Create a new superadmin
+            new_user = User(**new_user_data)
+            db.add(new_user)
+            db.commit()
+            db.refresh(new_user)
+
             print("Superadmin created")
         except Exception as error:
             print(f"Error creating superadmin: {error}")
