6cf21b088563789ee69201a86de999be5590c10c
Pristine Inc GH <pristineincgh@gmail.com>
2025-02-05 15:03:07 +0000

    update auth endpoints

--- admin-app/src/services/auth_service/auth_endpoints.ts
+++ admin-app/src/services/auth_service/auth_endpoints.ts
@@ -1,11 +1,16 @@
-import createAxiosInstance from '@/lib/axios-utils';
-import { LoginRequestInput } from '@/lib/validations/auth_validations';
-import { SessionSchema } from '@/types/auth_types';
+import createAxiosInstance from "@/lib/axios-utils";
+import {
+  LoginRequestInput,
+  ResetPasswordInput,
+} from "@/lib/validations/auth_validations";
+import { SessionSchema } from "@/types/auth_types";
+import { UserUpdateInput } from "@/types/user_types";
 
 const client = createAxiosInstance(false);
+const authClient = createAxiosInstance(true);
 
 export const LOGIN_USER = async (credentials: LoginRequestInput) => {
-  const response = await client.post('/auth/login', credentials);
+  const response = await client.post("/auth/login", credentials);
 
   const { access_token, refresh_token, user } = response.data;
 
@@ -16,4 +21,27 @@ export const LOGIN_USER = async (credentials: LoginRequestInput) => {
   } as SessionSchema;
 
   return userData;
+};
+
+// * This is the endpoint for resetting a user's password
+export const RESET_USER_PASSWORD = async (credentials: ResetPasswordInput) => {
+  const response = await client.post("/auth/reset-password", {
+    token: credentials.token,
+    new_password: credentials.password,
+  });
+
+  return response.data;
+};
+
+// * This is the endpoint for resending a password reset email
+export const RESEND_PASSWORD_RESET_EMAIL = async (token: string) => {
+  const response = await client.post("/auth/resend-reset-password", { token });
+
+  return response.data;
+};
+
+export const UPDATE_MY_PROFILE = async (user_data: UserUpdateInput) => {
+  const response = await authClient.patch("/users/me", user_data);
+
+  return response.data;
 };
