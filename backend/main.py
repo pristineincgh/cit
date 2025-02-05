098dd5ed869de830c721facb3f134045ca37ec47
Pristine Inc GH <pristineincgh@gmail.com>
2025-02-05 15:00:23 +0000

    update app entry file

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,16 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
 from app.routers.auth_routes import auth_router
 from app.routers.customer_routes import router as customer_router
 from app.routers.ticket_routes import router as ticket_router
+from app.routers.user_routes import user_router
 from app.utils.create_superadmin import create_superadmin_if_not_exists
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 version = "v1"
 api_prefix = f"/api/{version}"
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,20 +18,17 @@ async def lifespan(app: FastAPI):
     await create_superadmin_if_not_exists()
     yield  # Hand control back to the FastAPI app lifecycle
 
+
 app = FastAPI(
     title="Customer Issues Tracker (CIT)",
     description="A REST API for creating and managing customer tickets",
     version=version,
-    lifespan=lifespan
+    lifespan=lifespan,
 )
 
 
-
 # CORS middleware
-origins = [
-    "http://localhost:3000",
-    "https://cit-admin-two.vercel.app"
-]
+origins = ["http://localhost:3000", "https://cit-admin-two.vercel.app"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -38,9 +36,11 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["x-password-reset"],
 )
 
 app.include_router(router=auth_router, prefix=f"{api_prefix}/auth", tags=["auth"])
+app.include_router(router=user_router, prefix=f"{api_prefix}", tags=["users"])
 app.include_router(router=customer_router, prefix=f"{api_prefix}", tags=["customers"])
 app.include_router(router=ticket_router, prefix=f"{api_prefix}", tags=["tickets"])
 
