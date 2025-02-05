c9d7388ac56cbfc1b031bccbdf5d21fa3f65bad2
Pristine Inc GH <pristineincgh@gmail.com>
2025-02-05 15:02:49 +0000

    update auth mutations

--- admin-app/src/services/auth_service/auth_mutations.ts
+++ admin-app/src/services/auth_service/auth_mutations.ts
@@ -1,10 +1,15 @@
-import { useMutation } from '@tanstack/react-query';
-import { useRouter } from 'next/navigation';
-import { AxiosError } from 'axios';
-import toast from 'react-hot-toast';
-import { LOGIN_USER } from './auth_endpoints';
-import { createSession } from '@/actions/auth-actions';
-import { useAuthStore } from '@/store/authStore';
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { AxiosError } from "axios";
+import toast from "react-hot-toast";
+import {
+  LOGIN_USER,
+  RESEND_PASSWORD_RESET_EMAIL,
+  RESET_USER_PASSWORD,
+  UPDATE_MY_PROFILE,
+} from "./auth_endpoints";
+import { createSession } from "@/actions/auth-actions";
+import { useAuthStore } from "@/store/authStore";
 
 export const useLoginUser = () => {
   const router = useRouter();
@@ -16,12 +21,74 @@ export const useLoginUser = () => {
       try {
         await createSession(data);
         setSession(data);
-        router.push('/dashboard');
-        toast.success('Login successful');
+        router.push("/dashboard");
+        toast.success("Login successful");
       } catch (error) {
-        toast.error('Failed to login');
-        console.log('Error creating session:', error);
+        toast.error("Failed to login");
+        console.log("Error creating session:", error);
       }
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError && error.response) {
+        toast.error(error.response.data.detail);
+        const shouldResetPassword =
+          error.response.headers["x-password-reset"] === "true";
+
+        if (shouldResetPassword) {
+          router.push("/reset-password");
+        }
+      } else {
+        toast.error(`An error occurred: ${error.message}`);
+      }
+    },
+  });
+};
+
+export const useResetUserPassword = () => {
+  const router = useRouter();
+  return useMutation({
+    mutationFn: RESET_USER_PASSWORD,
+    onSuccess: (data) => {
+      toast.success(data.message);
+      router.push("/login");
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError && error.response) {
+        toast.error(error.response.data.detail);
+      } else {
+        toast.error(`An error occurred: ${error.message}`);
+      }
+    },
+  });
+};
+
+export const useResendPasswordResetEmail = () => {
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: RESEND_PASSWORD_RESET_EMAIL,
+    onSuccess: (data) => {
+      toast.success(data.message);
+      router.push("/resend-success");
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError && error.response) {
+        toast.error(error.response.data.detail);
+      } else {
+        toast.error(`An error occurred: ${error.message}`);
+      }
+    },
+  });
+};
+
+export const useUpdateMyProfile = () => {
+  const { updateProfile } = useAuthStore();
+
+  return useMutation({
+    mutationFn: UPDATE_MY_PROFILE,
+    onSuccess: (updated_user) => {
+      toast.success("Profile updated successfully");
+      updateProfile(updated_user);
     },
     onError: (error) => {
       if (error instanceof AxiosError && error.response) {
